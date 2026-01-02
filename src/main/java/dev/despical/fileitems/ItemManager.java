9c85379cf9e00aa613799057b54031b6522712db
Berke Akçen <berkeakcen@gmail.com>
2026-01-02 14:31:27 +0300

    Added license header

--- src/main/java/dev/despical/fileitems/ItemManager.java
+++ src/main/java/dev/despical/fileitems/ItemManager.java
@@ -1,3 +1,21 @@
+/*
+ * File Items - A library for loading items from YAMLs
+ * Copyright (C) 2026  Berke Akçen
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 package dev.despical.fileitems;
 
 import dev.despical.commons.XEnchantment;
@@ -35,7 +53,8 @@ public final class ItemManager {
     private Consumer<ItemBuilder> builderConsumer;
 
     public ItemManager(@NotNull JavaPlugin plugin) {
-        this(plugin, manager -> {});
+        this(plugin, manager -> {
+        });
     }
 
     public ItemManager(@NotNull JavaPlugin plugin, @NotNull Consumer<ItemManager> function) {
